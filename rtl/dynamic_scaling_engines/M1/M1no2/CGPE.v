6228a242babf556299dd607417722d79ab914f30
kooseo1296-debug <kooseo1296@gmail.com>
2026-08-17 11:03:08 +0900

    Add files via upload

---
+++ rtl/dynamic_scaling_engines/M1/M1no2/CGPE.v
@@ -0,0 +1,51 @@
+`include "param.v"
+
+module CGPE(
+    input CLK,
+    input RST,
+    input signed [`BIT_DATA-1:0] Input,
+    output reg [`BIT_DATA_C-1:0] Output
+    );
+    integer i;
+    wire [`BIT_DATA-1:0] Absolute;
+    wire [`EXP-1:0] Exponent, Exponent_0;
+    wire [`MAN-1:0] Mantissa, Mantissa_0;
+    wire [`MAN:0] Mantissa_1;
+    wire RoundUp;
+    
+    assign Absolute = (Input[`BIT_DATA-1]) ? -Input : Input;
+    
+    assign Exponent_0 = (Absolute[`BIT_DATA-1]) ? `EXP'd7: 
+                      (Absolute[`BIT_DATA-2]) ? `EXP'd6: 
+                      (Absolute[`BIT_DATA-3]) ? `EXP'd5: 
+                      (Absolute[`BIT_DATA-4]) ? `EXP'd4:
+                      (Absolute[`BIT_DATA-5]) ? `EXP'd3: 
+                      (Absolute[`BIT_DATA-6]) ? `EXP'd2:
+                      (Absolute[`BIT_DATA-7]) ? `EXP'd1: `EXP'd0;
+    
+    assign Mantissa_0 = (Exponent_0 == `EXP'd0) ? {1'b0,Absolute[`MAN-1:0]} :
+                      (Exponent_0 == `EXP'd1) ? {1'b0,Absolute[`MAN:1]} :
+                      (Exponent_0 == `EXP'd2) ? {1'b0,Absolute[`MAN+1:2]} : 
+                      (Exponent_0 == `EXP'd3) ? {1'b0,Absolute[`MAN+2:3]} : 
+                      (Exponent_0 == `EXP'd4) ? {1'b0, Absolute[`MAN+3:4]} : 
+                      (Exponent_0 == `EXP'd5) ? {1'b0, Absolute[`MAN+4:5]} :
+                      (Exponent_0 == `EXP'd6) ? {1'b0, Absolute[`MAN+5:6]} : {1'b0, Absolute[`MAN+6:7]};
+    
+    assign RoundUp = (Exponent_0 == `EXP'd1) ? Absolute[0] :
+                     (Exponent_0 == `EXP'd2) ? Absolute[1] : 
+                     (Exponent_0 == `EXP'd3) ? Absolute[2] : 
+                     (Exponent_0 == `EXP'd4) ? Absolute[3] : 
+                     (Exponent_0 == `EXP'd5) ? Absolute[4] : 
+                     (Exponent_0 == `EXP'd6) ? Absolute[5] : 1'b0;
+    
+    assign Mantissa_1 = Mantissa_0 + RoundUp;
+    
+    assign Mantissa = (Mantissa_1[`MAN]) ? Mantissa_1[`MAN:1] : Mantissa_1[`MAN-1:0];
+    assign Exponent = Mantissa_1[`MAN] + Exponent_0;
+                      
+    always @(posedge CLK) begin
+        if(~RST) Output <= {Input[`BIT_DATA-1], Exponent, Mantissa};
+        else Output <= `BIT_DATA_C'd0;
+    end
+    
+endmodule
---
+++ rtl/dynamic_scaling_engines/M1/M1no2/pe.v
@@ -0,0 +1,58 @@
+`include "param.v"
+
+module pe(
+    input CLK,
+    input [`BIT_ROW_ID-1:0] Row_ID,
+    input /*signed*/ [`BIT_DATA_C-1:0] Data_I_In, Data_W_In,
+    output reg signed [`BIT_DATA_C-1:0] Data_I_Out, Data_W_Out,
+    input EN_W_In,
+    output reg EN_W_Out,
+    input [`BIT_ROW_ID-1:0] EN_ID_In,
+    output reg [`BIT_ROW_ID-1:0] EN_ID_Out,
+    input signed [`BIT_PSUM-1:0] Psum_In,
+    output signed [`BIT_PSUM-1:0] Psum_Out,
+
+    input [`BIT_ADDR-1:0]   Addr_P_In,
+    input [`BIT_VALID-1:0]  Valid_P_In,
+    output reg [`BIT_ADDR-1:0]   Addr_P_Out,
+    output reg [`BIT_VALID-1:0]  Valid_P_Out
+    );
+
+
+reg /*signed*/ [`BIT_DATA_C-1:0] Data_W_Buf;
+
+wire Do_Compute = Valid_P_In & (Data_W_Buf[`MAN-1:0] != `MAN'd0);
+reg Do_Compute_1;
+
+reg signed [`BIT_PSUM-1:0] Psum_RAW;
+wire [`BIT_DATA*2-1:0] Psum_Multiplied;
+
+
+CGMultiplier Multiplier(
+    .CLK(CLK),
+    .Do_Compute(Do_Compute),
+    .Input(Data_I_In),
+    .Weight(Data_W_Buf),
+    .Output(Psum_Multiplied)
+    );
+    
+wire signed [2*`BIT_DATA-1:0] Gated_Multiplied = {(2*`BIT_DATA){Do_Compute_1}} & Psum_Multiplied;
+assign Psum_Out = Gated_Multiplied + Psum_RAW;
+
+always @(posedge CLK) begin
+    Data_I_Out <= Data_I_In;
+    Data_W_Out <= Data_W_In;
+    EN_W_Out <= EN_W_In;
+    EN_ID_Out <= EN_ID_In;
+    Addr_P_Out <= Addr_P_In;
+    Valid_P_Out <= Valid_P_In;
+    Do_Compute_1 <= Do_Compute;
+    
+    if (Valid_P_In) Psum_RAW <= Psum_In;
+    if (EN_W_In & (EN_ID_In == Row_ID)) Data_W_Buf <= Data_W_In;
+    
+end
+
+endmodule
+
+
